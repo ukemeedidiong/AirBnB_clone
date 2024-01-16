055030e47cda7bf16ddfd168477d4954044feffa
ukemeedidiong <ukemeedidiong226@gmail.com>
2024-01-16 03:18:01 -0800

    second commit

--- console.py
+++ console.py
@@ -3,7 +3,7 @@
 import cmd
 import re
 from shlex import split
-from models import storage
+from models.engine import file_storage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
